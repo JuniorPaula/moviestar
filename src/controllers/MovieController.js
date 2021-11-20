260dcfe15062330f77ea6ccf25ad3166904c74f5
Junior Paula <slucivaldo509@gmail.com>
2021-11-20 19:41:36 -0300

    create movie controller and save movie on database

--- src/controllers/MovieController.js
+++ src/controllers/MovieController.js
@@ -1,8 +1,30 @@
+const MovieModel = require('../models/MovieModel');
+
 exports.index = (req, res) => {
   res.render('newmovie');
 };
 
-exports.create = (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
+exports.create = async (req, res) => {
+  try {
+    if (!req.file) {
+      req.flash('errors', 'Selecione uma imagem para fazer o upload.');
+      req.session.save(() => res.redirect('/newmovie'));
+    }
+
+    const movie = new MovieModel(req.body, req.file);
+    const userID = req.session.user._id;
+
+    await movie.register(userID);
+
+    if (movie.errors.length > 0) {
+      req.flash('errors', movie.errors);
+      req.session.save(() => res.redirect('/newmovie'));
+      return;
+    }
+
+    req.flash('success', 'Filme cadastrado com sucesso!');
+    return req.session.save(() => res.redirect('/newmovie'));
+  } catch (e) {
+    console.log(e);
+  }
 };
